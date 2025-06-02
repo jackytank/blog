805ee254444686389c19364650e51912aedd7eba
jt <balisongian@gmail.com>
2025-06-02 23:33:26 +0700

    commit stuff

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-const allPosts = await queryCollection('content').order('date', 'DESC').all();
+const allPosts = await useAsyncData('allPosts', () => queryCollection('content').path('/blog').order('date', 'DESC').all());
 
 </script>
 
 <template>
+    
 </template>
 
 <style scoped>
